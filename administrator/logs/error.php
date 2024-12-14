058f47800d9984b8a062657665ca94923db9aa2e
SibayMaktay <nugumanovmuhammad@gmail.com>
2024-12-14 15:47:24 +0500

    v0.1_pc3

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-11-29 04:12:40 UTC
+#Software: Joomla! 5.1.4 Stable [ Kudumisha ] 27-August-2024 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2024-11-29T04:12:40+00:00	INFO ::1	joomlafailure	Логин и пароль не совпадают или такой учетной записи не существует.
